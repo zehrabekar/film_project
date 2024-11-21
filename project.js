30c94bc2b02499fe7b06ef120466ceb7c38052e3
Zehra Bekar <zehrabekar54@gmail.com>
2024-11-21 17:31:28 +0300

    Added functionality to display films on the UI

---
+++ project.js
@@ -0,0 +1,40 @@
+//form alanını ve inputları seçme :
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+
+//UI objesini başlatma
+const ui = new UI();
+
+//tüm eventleri yükleme 
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener("submit",addFilm); 
+}
+
+function addFilm(e){
+    const title =titleElement.value;
+    const director =directorElement.value;
+    const url =urlElement.value;
+
+    if(title === "" || director === "" || url === "") {
+        //Hata
+    }
+    else{
+        //yeni film
+        const newFilm = new Film(title,director,url);
+
+        ui.addFilmToUI(newFilm); // Arayüze film ekleme
+    }
+
+
+    ui.clearInputs(titleElement,urlElement,directorElement);
+
+    e.preventDefault();
+}
+
+
+
---
+++ ui.js
@@ -0,0 +1,41 @@
+// arayüze filmleri eklemek için UI Constructor'a ihtiyacımız var
+function UI(){
+
+}
+// herhangi bir özelliği olamayacak sadece prototipine fonksiyon yazıcaz
+
+UI.prototype.addFilmToUI = function(newFilm){
+
+    /* <tr>
+        <td><img src="" class="img-fluid img-thumbnail"></td>
+        <td></td>
+        <td></td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+      </tr> */
+
+    const filmList = document.getElementById("films");
+    filmList.innerHTML += `
+    <tr>
+        <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+        <td>${newFilm.title}</td>
+        <td>${newFilm.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+      </tr>
+    `;
+}
+
+UI.prototype.clearInputs = function(element1,element2,element3){
+    element1.value = "";
+    element2.value = "";
+    element3.value = "";
+}
+
+//Fonksiyonların prototipte tanımlanması, her UI nesnesi için yeniden oluşturulmasını engeller.
+//Eğer clearInputs fonksiyonu her UI nesnesi içinde tanımlansaydı, bellekte aynı işlev için birden fazla kopya tutulmuş olurdu.
+
+
+
+
+
+
+
