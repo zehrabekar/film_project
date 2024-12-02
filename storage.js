b87dfce793dcfcbecdd068838e903982faef3827
Zehra Bekar <zehrabekar54@gmail.com>
2024-12-02 16:56:17 +0300

    Added functionality to load films from localStorage and display them on the UI.

--- storage.js
+++ storage.js
@@ -2,24 +2,26 @@ function Storage(){
 
 }
 Storage.prototype.addFilmToStorage = function(newFilm){
-    let films = this.getFilmsFromStorage();
+    let films = this.getFilmsFromStorage();// Önceden kaydedilen filmleri al
 
-    films.push(newFilm);
+    films.push(newFilm);// Yeni filmi mevcut listeye ekle
 
-    localStorage.setItem("films",JSON.stringify(films));
-    
+    localStorage.setItem("films",JSON.stringify(films));// Güncellenmiş listeyi localStorage'a kaydet
+
 }   
 
 Storage.prototype.getFilmsFromStorage = function(){
-    let films;
+    let films;// Film listesi tanımlandı
 
-    if(localStorage.getItem("films") === null){
-        films=[];
+    if(localStorage.getItem("films") === null){// Eğer localStorage'da "films" anahtarı yoksa
+        films=[];// Boş bir dizi başlat
 
     }
     else{
-        films=JSON.parse(localStorage.getItem("films"));
+        films=JSON.parse(localStorage.getItem("films"));//Eğer veri varsa, JSON formatından bir JavaScript nesnesine dönüştürülür ve döndürülür. 
     }
 
-    return films;
+    return films; // Film listesini döndür
 }
+
+//JSON formatından JavaScript nesnesine dönüştürmek, bir metin (string) olarak saklanan JSON verisini, JavaScript'in kullanabileceği bir nesne veya dizi yapısına çevirmek anlamına gelir.
